6c15f796a805ad9398b549a1157acd839ae1ecc2
inksnw <inksnw@gmail.com>
2021-02-24 19:12:43 +0800

    减少进程信息,排除可能的错误

--- core/psutil.go
+++ core/psutil.go
@@ -2,9 +2,9 @@ package core
 
 import (
 	"fmt"
+	"github.com/phuslu/log"
 	"github.com/shirou/gopsutil/v3/cpu"
 	"github.com/shirou/gopsutil/v3/mem"
-	"github.com/shirou/gopsutil/v3/process"
 	"strconv"
 	"time"
 )
@@ -18,32 +18,25 @@ func GetPcInfo() (info PcInfo) {
 		if !ok {
 			continue
 		}
-		proIns := process.Process{Pid: int32(pid)}
-
-		cpuper, _ := proIns.CPUPercent()
-		cpuper1, _ := strconv.ParseFloat(fmt.Sprintf("%.2f", cpuper), 64)
-
-		Threads, _ := proIns.NumThreads()
-		t, _ := proIns.MemoryInfo()
-		f, _ := proIns.OpenFiles()
-
 		rv = append(rv, ProcessInfo{
 			TaskId:  item.Key,
-			Mem:     t.RSS,
 			Pid:     int32(pid),
-			Threads: Threads,
-			Files:   len(f),
-			CpuPer:  cpuper1,
 		})
 	}
 	info.ProStatus = rv
 	info.Version = Version
 	info.ProNum = len(rv)
-	m, _ := mem.VirtualMemory()
+	m, err := mem.VirtualMemory()
+	if err!=nil{
+		log.Error().Msgf("获取内存占用失败: %s", err)
+	}
 	info.MemTotal = m.Total
 	info.MemUsed = m.Used
-	cpu, _ := cpu.Percent(3*time.Second, false)
-	cpu1, _ := strconv.ParseFloat(fmt.Sprintf("%.2f", cpu[0]), 64)
+	cpuOri, err := cpu.Percent(3*time.Second, false)
+	if err!=nil{
+		log.Error().Msgf("获取cpu占用失败: %s", err)
+	}
+	cpu1, _ := strconv.ParseFloat(fmt.Sprintf("%.2f", cpuOri[0]), 64)
 	info.TotalPercent = cpu1
 
 	return info
